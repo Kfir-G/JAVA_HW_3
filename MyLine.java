c89f37a6b049d41c21a28fbf43c14d1ed9bf2722
Kfir-G <kfirg15@gmail.com>
2020-12-09 16:06:00 +0200

    add JavaDoc to MyLine class

--- MyLine.java
+++ MyLine.java
@@ -2,19 +2,32 @@ import java.awt.Color;
 import java.awt.Graphics;
 public class MyLine extends MyShape
 {
-    //------data fields------
-
     //------constructors-------
+    /**
+     * MyLine no-argument constructor, initializes vales data to 0 and color to black
+     * Call to super(MyShape) constructor
+     */
     public MyLine()
     {
         super(); 
     }
+    /**
+     * MyLine constructor with parameters
+     * @param x1 the value of X of the first dot
+     * @param y1 the value of y of the first dot
+     * @param x2 the value of X of the second dot
+     * @param y2 the value of y of the second dot
+     * @param myColor the value of the shape's colo
+     */
     public MyLine(int x1, int y1, int x2, int y2, Color myColor)
     {
         super(x1,y1,x2,y2,myColor);
     }
 
     //------methods---------
+    /**
+     * draw method which draws the outline a line
+     */
     public void draw(Graphics g)
     {
         g.setColor(getColor());

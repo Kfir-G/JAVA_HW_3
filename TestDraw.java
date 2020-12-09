f312fac02e37239e03f62b3e9bbd458d032a5908
Kfir-G <kfirg15@gmail.com>
2020-12-09 10:37:07 +0200

    add exit

--- TestDraw.java
+++ TestDraw.java
@@ -14,18 +14,17 @@ public class TestDraw
             input = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter Number of shapes:"));
         }
         catch (NullPointerException e1){
-            return; //EXIT the program
+            System.exit(1); //EXIT the program
         }
         catch(NumberFormatException e2){
             JOptionPane.showMessageDialog(null,"Invalid number: "+input);
-            return;
+            System.exit(1);
         }
         DrawPanel panel = new DrawPanel(input);
 
         JLabel statusLabel = new JLabel(panel.getLabelText()); // create a JLabel containing the shape information
 
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         application.add(panel); // add drawing to CENTER by default
         application.add(statusLabel, BorderLayout.SOUTH); // add the status label to the SOUTH (bottom) of the frame
         application.setSize(500, 500);
